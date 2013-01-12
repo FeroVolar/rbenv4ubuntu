f1a93ce1f3c26224e76645a7230aa5a6f2bb457f
Fero Volar <volar.frantisek@gmail.com>
2013-01-12 22:39:46 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@
 ## rbenv4ubuntu
 ## version 0.1
 ## This Bash script install Ruby on Rails with rbenv on Ubuntu.
-## - run as standard user
-## - environment is created in the home directory
 ##
 
 
